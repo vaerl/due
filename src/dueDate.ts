261234ee615310efb894a7a6d610c2265647042c
vaerl <lukas@mlel.dev>
2021-04-15 18:33:43 +0200

    Add basic model.

---
+++ src/dueDate.ts
@@ -0,0 +1,9 @@
+import { Uri } from "vscode";
+
+export class DueDate {
+	public date: Date;
+
+	constructor(public readonly file: Uri, public readonly value: string) {
+		this.date = new Date(value);
+	}
+}
