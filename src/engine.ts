d7eab75351986e161b01b5899e3959cf5e1f7ae5
vaerl <lukas@mlel.dev>
2021-04-18 19:44:43 +0200

    Add discardDate().

--- src/engine.ts
+++ src/engine.ts
@@ -107,4 +107,13 @@ export class Engine {
 			}
 		});
 	}
+
+	discardDates(file: Uri) {
+		for (let index = 0; index < this.dueDates.length; index++) {
+			const date = this.dueDates[index];
+			if (date.uri.path === file.path) {
+				this.dueDates.splice(index, 1);
+			}
+		}
+	}
 }
