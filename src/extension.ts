cb41e0f3318c81a7a4049569207e6d833eb43e6b
vaerl <lukas@mlel.dev>
2021-04-18 10:30:12 +0200

    Fix typo.

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let engine = new Engine();
 
 	let scanWorkspace = vscode.commands.registerCommand(
-		"due.scanWorkSpace",
+		"due.scanWorkspace",
 		() => {
 			engine.scanWorkspace();
 		}
