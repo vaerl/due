fa88e3259154a67395caabfb5e6296b925d4f3d8
vaerl <lukas@mlel.dev>
2021-04-24 21:59:33 +0200

    Fill tree-views and use config.

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { DueConfig } from "./config";
+import { DueDateProvider } from "./dueDateProvider";
 import { Engine } from "./engine";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.debug("Starting due.");
-	let engine = new Engine();
-	engine.scanWorkspace();
+	let dueDateProviders = DueConfig.getCategories().map(
+		(conf) => new DueDateProvider(conf.title)
+	);
+	let engine = new Engine(dueDateProviders);
 
 	let scanWorkspace = vscode.commands.registerCommand(
 		"due.scanWorkspace",
@@ -27,6 +31,20 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.workspace.onWillSaveTextDocument((_event) => {
 		engine.scanFile(engine.getOpenEditor().document.uri);
 	});
+
+	// TreeView-stuff
+	let counter = 1;
+	dueDateProviders.forEach((provider) => {
+		let id = "due-" + counter++;
+		let treeView = vscode.window.createTreeView(id, {
+			treeDataProvider: provider,
+		});
+		treeView.title = provider.status;
+		context.subscriptions.push(treeView);
+	});
+
+	// scan after everything is set up
+	engine.scanWorkspace();
 }
 
 // this method is called when your extension is deactivated
